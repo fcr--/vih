745fd320944d25b6592b2297f4151171e5b23e48
Francisco Castro <fcr@adinet.com.uy>
2011-11-04 22:56:21 -0200

    Termino intérprete de postscript!!!
      * Ya funciona y todo:
    
    *PSvi> let Right program = psParse "3 2 pop 1" in psInterp psNewState program
    Right (PSState {globDict = fromList [(PSString "pop",PSInternalOp pop)], dictStack = [], stack = [PSInt 1,PSInt 3]})

--- src/PSvi.hs
+++ src/PSvi.hs
@@ -1,13 +1,51 @@
-module PSvi where
+module PSvi (psParse,
+             PSObject(..),
+             psNewState,
+             psInterp)
+    where
 
 import Peg.Peg
 import Char
+import Control.Applicative
+import qualified Data.Map as M
 
 data PSObject = PSString String
               | PSCode [PSObject]
               | PSInt Int
               | PSName String
-  deriving Show
+              | PSMap (M.Map PSObject PSObject)
+              | PSInternalOp String (PSState -> IO (Either String PSState))
+
+instance Show PSObject where
+    show (PSString s)       = "PSString " ++ show s
+    show (PSCode c)         = "PSCode " ++ show c
+    show (PSInt i)          = "PSInt " ++ show i
+    show (PSName n)         = "PSName " ++ show n
+    show (PSMap m)          = "PSMap " ++ show m
+    show (PSInternalOp n _) = "PSInternalOp " ++ n
+
+instance Eq PSObject where
+    PSString s == PSString t             = s == t
+    PSCode c1 == PSCode c2               = c1 == c2
+    PSInt i == PSInt j                   = i == j
+    PSName n == PSName m                 = n == m
+    PSMap m1 == PSMap m2                 = m1 == m2
+    PSInternalOp o _ == PSInternalOp p _ = o == p
+    _ == _                               = False
+
+instance Ord PSObject where
+    compare (PSString s1) (PSString s2)              = compare s1 s2
+    compare (PSString _) _                           = LT
+    compare (PSCode c1) (PSCode c2)                  = compare c1 c2
+    compare (PSCode _) _                             = LT
+    compare (PSInt i1) (PSInt i2)                    = compare i1 i2
+    compare (PSInt _) _                              = LT
+    compare (PSName n1) (PSName n2)                  = compare n1 n2
+    compare (PSName _) _                             = LT
+    compare (PSMap m1) (PSMap m2)                    = compare m1 m2
+    compare (PSMap _) _                              = LT
+    compare (PSInternalOp o1 _) (PSInternalOp o2 _)  = compare o1 o2
+    compare (PSInternalOp _ _) _                     = LT
 
 -- ps = blanks (atom blanks)*
 grm_ps = PegAster PSCode (
@@ -86,5 +124,69 @@ psParse text = case res of
     errInfo' = take n text
     ttext = take len (drop n text) ++ " "
     len = 15
+
+
+
+------ POSTSCRIPT INTERPRETER ------
+
+data PSState = PSState {
+        globDict  :: M.Map PSObject PSObject,
+        dictStack :: [M.Map PSObject PSObject],
+        stack     :: [PSObject]}
+    deriving Show
+
+
+
+psNewState :: PSState
+psNewState = PSState {
+        globDict = M.fromList globals,
+        dictStack = [],
+        stack = []}
+    where
+    globals = map (\(k,v) -> (PSString k, PSInternalOp k v)) [
+        ("pop", psOpPop)]
+
+
+
+psInterp :: PSState -> PSObject -> IO (Either String PSState)
+
+-- psInterp for string objects (push):
+psInterp st obj@(PSString s) = return $ Right st {stack = obj : stack st}
+
+-- psInterp for code objects:
+psInterp st obj@(PSCode (o:os)) = do
+        e <- psInterp st o
+        case e of
+            Left m -> return e
+            Right st' -> psInterp st' (PSCode os)
+
+-- psInterp for empty code objects:
+psInterp st obj@(PSCode []) = return $ Right st
+
+-- psInterp for int objects (push):
+psInterp st obj@(PSInt i) = return $ Right st {stack = obj : stack st}
+
+-- psInterp for name objects (lookup):
+psInterp st (PSName n) = case find n (dictStack st ++ [globDict st]) of
+        Just obj -> psInterp st obj
+        Nothing -> return $ Left ("psInterp error: name " ++ n ++ " not found.")
+    where
+    find n (d:ds) = M.lookup (PSString n) d <|> find n ds
+    find n [] = Nothing
+
+-- psInterp for map objects (push):
+psInterp st obj@(PSMap m) = return $ Right st {stack = obj : stack st}
+
+-- psInterp for internal objects (execute):
+psInterp st obj@(PSInternalOp name op) = op st
+
+
+
+------ POSTSCRIPT STACK OPERATORS ------
+
+psOpPop :: PSState -> IO (Either String PSState)
+psOpPop st = return $ case stack st of
+    (s:ss) -> Right st {stack = ss}
+    [] -> Left "psInterp error: empty stack."
 
 -- vi: et sw=4
