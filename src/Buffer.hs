cb285fad7f5c169e2002c7ffb6099c854a0f12df
Pablo Rotondo <pablo_edu_31@hotmail.com>
2011-12-04 14:23:42 -0200

    Buffer

--- src/Buffer.hs
+++ src/Buffer.hs
@@ -50,17 +50,34 @@ lineDown buff
 getLine :: Buffer -> BufferLine
 getLine buff = let (p,c,n) = contents buff in c
 
--- updateLine :: Buffer -> BufferLine -> Buffer
+updateLine :: Buffer -> BufferLine -> Buffer
+updateLine buff@(Buffer (p,_,s) _ _) cr		=	buff { contents = (p,cr,s) }
 
--- deleteLine :: Buffer -> Buffer
+-- dd
+deleteLine :: Buffer -> Buffer
+deleteLine (Buffer (pr,l,sig) crLine numLines)	|	numLines == 1	=	newBuf
+						|	null sig	=	Buffer ( init pr, last pr, sig) (crLine-1) (numLines-1)
+						|	otherwise	=	Buffer ( pr, head sig, tail sig) crLine (numLines-1)
 
 -- equivalent to pressing 'O' (false) or 'o' (true).
--- openLine :: Buffer -> Bool -> Buffer
--- openLine buff after = ...
+openLine :: Buffer -> Bool -> Buffer
+openLine buff after 	|	after		=	buff { contents = oT (contents buff), curLine = (curLine buff + 1), numLines = (numLines buff + 1) }
+			|	otherwise	=	buff { contents = oF (contents buff), numLines = (numLines buff + 1) }
+	where
+		oT	=	\(prev, l, sig) -> ( prev ++ [l], BufferLine "", sig)
+		oF	=	\(prev, l, sig) -> ( prev , BufferLine "", l : sig)
 
 -- equivalent to pressing J in normal mode of vim.
--- joinLine :: Buffer -> Buffer
-
+joinLine :: Buffer -> Buffer
+joinLine buff	=	doIt (contents buff)
+	where
+		doIt	= \(prev, l@(BufferLine ls), sig) -> case sig of
+--						Non-empty line, add a space in the middle
+						(BufferLine s@(_:_) ):ss -> buff { contents = (prev, BufferLine (ls ++ (' ':s) ), ss) , numLines = (numLines buff - 1) }
+--						Empty line, consume line
+						(BufferLine _ ):ss -> buff { contents = (prev, l, ss) , numLines = (numLines buff - 1) }
+--						No next line... nothing
+						_		  -> buff
 
 ------ BufferLine functions ------
 
