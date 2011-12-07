3b9997ab5717ce9ec5236c74bd4f51feb4436041
Esteban <nocrala.nabetse@gmail.com>
2011-12-07 14:26:10 -0200

    Cursor implemented for the command line.

--- src/Terminal.hs
+++ src/Terminal.hs
@@ -23,6 +23,8 @@ data WTManager = WTMa {lo :: Layout -- current layout
                       ,vty :: Vty
                       }
 
+data CommandLine = CM {comm :: String, pos :: Int}
+
  -- The way to initialize the window manager at program startup
  -- TODO: change wsizes, Window initializer, possibly add a startup
  -- size variable
@@ -62,6 +64,7 @@ data Layout = Vspan Int Int [(Layout,Int)] --Vertical span with sizes
              |Window {size::(Int,Int), buffNum::Int} -- Window with buffer number
              |NoWin{size :: (Int,Int)}
              deriving(Show)
+
 --To navigate through the layout
 bgcolor = def_attr `with_fore_color` red
 dum = def_attr `with_style` reverse_video
@@ -70,6 +73,7 @@ cbarras = def_attr `with_style` reverse_video `with_fore_color` red
 --Function to resize the current layout to fit the new window size
 resizeLayout::Int->Int->WTManager->WTManager
 resizeLayout h w wtm = wtm{lo = (resizeLo h w) (lo wtm)}
+
 resizeLo :: Int -> Int -> Layout -> Layout
 resizeLo w h lo = case lo of
                     (Vspan px py xs) -> Vspan w h $(funcV w h xs)
@@ -80,72 +84,79 @@ resizeLo w h lo = case lo of
         funcV w h lst = let (d,m) = divMod h (length lst) in map (\(l,lst) -> (resizeLo w (d+1) l,d+1)) (take m lst) ++ map (\(l,lst) -> (resizeLo w d l,d)) (drop m lst)
         funcH w h lst = let (d,m) = divMod w (length lst) in map (\(l,lst) -> (resizeLo (d+1) h l,d+1)) (take m lst) ++ map (\(l,lst) -> (resizeLo d h l,d)) (drop m lst)
 
+--Get the terminal's height
 getH :: DisplayRegion -> Word
 getH (DisplayRegion w h) = h
 
+--Get the terminal's weight
 getW :: DisplayRegion -> Word
 getW (DisplayRegion w h) = w
 
+--Function to print the vty contained in the wtm
 showWTM :: WTManager -> IO ()
 showWTM wtm = let vty' = (vty wtm) in
               do bnds <- display_bounds (terminal vty')
                  printloop vty' wtm (getW bnds) (getH bnds)
 
+--Function to get the next event from the Vty
 getKey :: WTManager -> IO Event
 getKey wtm = next_event (vty wtm)
 
+--Function to get a command from the command line
 getCommand :: WTManager -> IO (Maybe String)
 getCommand wtm = let vty' = (vty wtm) in
                  do bnds <- display_bounds (terminal vty')
-                    getCommand' ":" wtm (getW bnds) (getW bnds)
+                    getCommand' line wtm (getW bnds) (getW bnds)
+    where line = CM {comm = ":", pos = 1}
 
-getCommand' :: String -> WTManager -> Word -> Word -> IO (Maybe String)
-getCommand' s wtm w h= do nEv <- next_event (vty wtm)
-                          case nEv of
-                            EvKey (KASCII c) _ -> let newS = s ++ [c] 
-                                                  in do update (vty wtm) (pic_for_image $ armarCommand newS (fromIntegral w) (fromIntegral h) wtm)
-                                                        getCommand' newS wtm w h
-                            EvKey (KEnter) _ -> do update (vty wtm) (pic_for_image $ armarCommand "Ejecutando" (fromIntegral w) (fromIntegral h) wtm)
-                                                   return (Just s)
-                            EvKey (KBS) _ -> let newS = init s
-                                             in do update (vty wtm) (pic_for_image $ armarCommand newS (fromIntegral w) (fromIntegral h) wtm)
-                                                   getCommand' newS wtm w h
-                            EvKey (KLeft) _ -> getCommand' s wtm w h --TODO:all
-                            EvKey (KRight) _ -> getCommand' s wtm w h --TODO:all
-                            EvKey (KEsc) _ -> do printloop (vty wtm) wtm (fromIntegral w) (fromIntegral h)
-                                                 return Nothing
-                            EvResize nx ny -> do printloop (vty wtm) (resizeLayout nx ny wtm) (fromIntegral nx) (fromIntegral ny-1)
-                                                 getCommand' s wtm w h
-                            _ -> do update (vty wtm) (pic_for_image $ armarCommand s (fromIntegral w) (fromIntegral h) wtm)
-                                    getCommand' s wtm w h
+--Function that asks continully for the next character of the command until it ends and return the string that composes it
+getCommand' :: CommandLine -> WTManager -> Word -> Word -> IO (Maybe String)
+getCommand' cl wtm w h= do nEv <- next_event (vty wtm)
+                           case nEv of
+                             EvKey (KASCII c) _ -> let newCL = addCharComm cl c
+                                                   in do update (vty wtm) (pic_for_image $ armarCommand (comm newCL) (fromIntegral w) (fromIntegral h) wtm)
+                                                         getCommand' newCL wtm w h
+                             EvKey (KEnter) _ -> do update (vty wtm) (pic_for_image $ armarCommand "Ejecutando" (fromIntegral w) (fromIntegral h) wtm)
+                                                    return (Just $ comm cl)
+                             EvKey (KBS) _ -> let newS = init $ comm cl
+                                                  newP = pos cl -1
+                                              in do update (vty wtm) (pic_for_image $ armarCommand newS (fromIntegral w) (fromIntegral h) wtm)
+                                                    getCommand' (cl {comm = newS, pos = newP }) wtm w h
+                             EvKey (KLeft) _ -> let cur = pos cl
+                                                in if cur == 0
+                                                   then getCommand' cl wtm w h
+                                                   else getCommand' (cl {pos = cur - 1}) wtm w h
+                             EvKey (KRight) _ -> let cur = pos cl
+                                                 in if cur == length (comm cl) - 1
+                                                    then getCommand' cl wtm w h
+                                                    else getCommand' (cl {pos = cur + 1}) wtm w h 
+                             EvKey (KEsc) _ -> do printloop (vty wtm) wtm (fromIntegral w) (fromIntegral h)
+                                                  return Nothing
+                             EvResize nx ny -> do printloop (vty wtm) (resizeLayout nx ny wtm) (fromIntegral nx) (fromIntegral ny-1)
+                                                  getCommand' cl wtm w h
+                             _ -> do update (vty wtm) (pic_for_image $ armarCommand (comm cl) (fromIntegral w) (fromIntegral h) wtm)
+                                     getCommand' cl wtm w h
+
+addCharComm :: CommandLine -> Char -> CommandLine
+addCharComm cl c = let (a,b) = splitAt (pos cl + 1) (comm cl)
+                       i = pos cl
+                   in cl {comm = a++[c]++b, pos = i+1}
                       
-
+--Function that given a vty prints it on the screen
 printloop :: Vty -> WTManager -> Word -> Word -> IO ()
-printloop vty wtm w h= do
-                    update vty (pic_for_image $ armarIm (fromIntegral w) (fromIntegral h) wtm)
+printloop vty wtm w h = do update vty (pic_for_image $ armarIm (fromIntegral w) (fromIntegral h) wtm)
 
+--Function that paints the command line with the accumulative string given
 armarCommand s w h wtm = if h<4 then string def_attr "No se puede visualizar con una altura de menos de 4 caracteres"
                          else (printWTM wtm) <-> string def_attr s
                        
 
+--Function that paints an empty window
 armarIm w h wtm =if h<4 then string def_attr "No se puede visualizar con una altura de menos de 4 caracteres"
                  else bordeSuperior w
                       <->(printWTM wtm)
                       <->bordeInferior w
-{-
-
-data WTManager = WTMa {lo :: Layout -- current layout
-                      ,curwdw :: [Int] -- current window being used
-                      ,wtmH :: Int -- height in characters of window
-                      ,wtmW :: Int -- width in characters of window
-                      ,bm :: BManager
-
-data Layout = Vspan Int Int [(Layout,Int)] --Vertical span with sizes
-             |Hspan Int Int [(Layout,Int)] --Horizontal span with sizes
-             |Window {size::(Int,Int), buffNum::Int} -- Window with buffer number
-             |NoWin {size:: (Int,Int)}
-
--}                     
+                    
 bordeSuperior w = char_fill dum '-' w 1
 bordeInferior w = char_fill dum '-' w 2
 -- TODO: mirar...
@@ -173,15 +184,18 @@ printLayout lOut = case lOut of
 --Asociar buffer a ventana
 assocBuffer :: Int -> [Int] -> WTManager -> WTManager
 assocBuffer bId wId wtm = wtm{lo = asBuf bId wId (lo wtm)}
+
 asBuf :: Int -> [Int] -> Layout -> Layout
 asBuf bId (w:ws) lo = case lo of
                         (Window (x,y) z) -> Window (x,y) bId
                         (NoWin (x,y)) -> Window (x,y) bId
                         (Vspan w h lst) -> Vspan w h (take w lst ++ [(\(l,s) -> (asBuf bId ws l,s)) (lst!!w)] ++ drop (w+1) lst)
                         (Hspan w h lst) -> Hspan w h (take w lst ++ [(\(l,s) -> (asBuf bId ws l,s)) (lst!!w)] ++ drop (w+1) lst)
+
 --Hacer split Horizontal con param = True, vertical con param = False
 splitX :: Bool -> WTManager -> WTManager
 splitX param wtm = wtm{ lo = splitLoX param (lo wtm) cw, curwdw = (\xs -> init xs ++ (map (+1) [last xs])) cw } where cw = curwdw wtm
+
 splitLoX :: Bool -> Layout -> [Int] -> Layout
 splitLoX param l (x: xs@(y':ys)) = case l of
                 (Vspan w h lst) -> Vspan w h ((take x) lst ++ [(\(lay,height) -> (splitLoX param lay xs,height)) (lst!!x)] ++ drop (x+1) lst)
