d8afef3e016c6a53af8efa1d1154754373db1671
Pablo <pablo_edu_31@hotmail.com>
2011-12-07 11:18:01 -0200

    BufferManager

--- src/BufferManager.hs
+++ src/BufferManager.hs
@@ -1,6 +1,6 @@
-module BufferManager(BM,runBM,BManager,currentBuffer,nextBuffer,modBuffer,insBuffer,prevBuffer,initBM) where
+module BufferManager(newBM,newBuffer,getLineBM,setLineBM,getBuffSizeBM,setYposBM) where
 import qualified Buffer as B
-import Data.Map(Map,(!),singleton,keys,insert,lookup,size)
+import Data.Map(Map,(!),singleton,keys,insert,lookup,size,empty)
 import Data.Maybe(fromJust)
 import qualified Buffer as Buffer
 import System.IO
@@ -9,64 +9,29 @@ import Prelude hiding (lookup)
 
 data BManager = BManager { 
                            buffers :: Map Int B.Buffer
-                          ,curBuffer :: Int -- current buffer we're standing on
                           ,maxbuffer :: Int -- highest identifier given in a session
-                          ,vNumber :: Int -- version number: updates every time the BM is modified
-                         } deriving(Show)
+                         } deriving Show
 
-newtype BM a = BM {runBM :: BManager -> (BManager, a)} -- monad hiding all this
-instance Monad BM where
- --(>>=) :: BM a -> (a -> BM b) -> BM b 
- b >>= c = BM (\bm-> let (interBM,a) = runBM b bm in
-                 (runBM (c a)) interBM )
- --return :: a -> BM a               
- return a = BM (\bm -> (bm,a))
 
 --TODO: change all functions to actually do something
 ------ functions over BM ------
 
+-- Constructor
 
---adding a buffer object to the BM, addds 1 to maxbuffer, adds the buffer to the bufferdata list
-insBuffer :: B.Buffer -> BM ()
-insBuffer newbuf = BM (\bm->(bm {buffers = insert (maxbuffer bm) newbuf (buffers bm) 
-                           ,maxbuffer=1+maxbuffer bm,vNumber = 1+vNumber bm},()))
+newBM :: BManager
+newBM = BManager empty 0
 
---substitutes current buffer with the one given
-modBuffer :: B.Buffer -> BM ()
-modBuffer buf = BM (\bm -> (bm{buffers = insert (curBuffer bm) buf (buffers bm),vNumber = 1+vNumber bm},()))
+-- Creates a ne(BM,runBM,BManager,currentBuffer,nextBuffer,modBuffer,insBuffer,prevBuffer,initBM)w buffer 
+newBuffer :: BManager -> Maybe FilePath -> IO (BManager, Int)
+newBuffer bm Nothing	=	let bm' = insBuffer bm B.newBuf in return (bm', maxbuffer bm' - 1)
+newBuffer bm (Just fp)	=	(>>=) (Buffer.readFile fp) $  \b -> let bm' = insBuffer bm b in return (bm', maxbuffer bm' - 1)
 
---getting the current buffer from memory, searches with the currentbuffer as key
-currentBuffer :: BM B.Buffer
-currentBuffer = BM (\bm -> (bm {vNumber = 1+vNumber bm}, getCurBuff bm))
-    where getCurBuff bm = (buffers bm)!(curBuffer bm)
 
---get the next buffer identifier and switch to next buffer
-nextBuffer :: BM Int
-nextBuffer = BM (\bm -> let allkeys = (keys.buffers) bm
-                            hkeys = filter (> curBuffer bm) allkeys
-                            finalkey   = if (not.null) hkeys then head hkeys else head allkeys in
-                            (bm{curBuffer = finalkey, vNumber = 1+vNumber bm},finalkey))
-
---get the previous buffer identifier and switch to the previous buffer
-prevBuffer :: BM Int
-prevBuffer = BM (\bm -> let allkeys = (keys.buffers) bm
-                            lkeys = filter (< curBuffer bm) allkeys
-                            finalkey = if (not.null) lkeys then (head.reverse) lkeys else (head.reverse) allkeys in
-                            (bm{curBuffer = finalkey, vNumber = 1+vNumber bm},finalkey))
-
---switch to arbitrary buffer and return True if the buffer existed
-swBuffer :: Int -> BM Bool
-swBuffer arbBuff = BM (\bm -> let (nb,i) = case (lookup arbBuff.buffers) bm of
-                                        Nothing -> (curBuffer bm, 0)
-                                        (Just b)-> (arbBuff, 1) 
-                              in
-                              (bm{curBuffer = nb,vNumber = i+vNumber bm},curBuffer bm == nb))
-
-initBM :: BManager -- Creates a new single buffer with no associated file
-initBM = BManager { buffers = singleton 0 B.newBuf
-                  , curBuffer = 0, maxbuffer = 1, vNumber = 0}
-
--- vi: et sw=4
+-- internal function
+insBuffer :: BManager -> B.Buffer -> BManager
+insBuffer bm buff	=	bm { buffers = insert (maxbuffer bm) buff buffer, maxbuffer = maxbuffer bm + 1}
+	where
+		buffer = buffers bm
 
 -- Number of buffers.
 getBuffSizeBM :: BManager  -> Int
@@ -99,5 +64,4 @@ setYposBM bm bn line = case lookup bn mp of
 		mp = buffers bm
 
 --openFile :: BManager -> String -> BManager
-
 
